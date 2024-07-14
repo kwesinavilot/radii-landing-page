0803c36de1690653d38db1b920ad5bbe2560ca72
Ibukun Ayomide-Baafog <ayomideracheal12@gmail.com>
2024-07-14 20:21:32 +0000

    functionality to button

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,19 +3,30 @@ import styles from '../styles/HeroSection.module.css';
 import { BsPlayCircle } from "react-icons/bs";
 import { Card } from 'react-bootstrap'; // or your preferred library
 import { Button } from './ui/button';
+import { useState } from 'react';
 
 const HeroSection = () => {
+  const [herobutton, setButton] = useState(false);
   return (
     <section className="lg:flex">
       <div className="flex flex-col lg:flex-row z-10 items-center relative lg:flex gap-4 lg:gap-0">
         <div className="lg:w-[50%] lg:px-32 mx-auto text-start lg:text-start px-4 py-10">
-          <p className="text-[#F27405] font-light">UNLOCK YOUR BUSINESS POTENTIAL           </p>
+          <p className="text-[#F27405] font-light">UNLOCK YOUR BUSINESS POTENTIAL</p>
           <h1 className="text-2xl mt-4 lg:text-6xl font-bold relative after:absolute after:content-[''] after:top-0 after:bg">Eliminate Guesswork,<br />Drive Growth</h1>
           <p className="lg:my-8 my-2">Stop relying on trial and error. Integrate all your data sources to unlock actionable insights and make strategic decisions that propel your business forward.</p>
           
           <div className="bg-white shadow-lg absolute left-0 lg:w-[40%] p-4 lg:ps-[8rem] rounded-ee-[1rem] rounded-se-[1rem]">
             <div className="flex gap-12">
-              <Button className="bg-[#F27405] text-white rounded-lg shadow-lg">Try It Now</Button>
+              <a
+                onClick={() => setButton(!herobutton)}
+                href="https://app.getradii.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-[#F27405] text-white px-4 cursor-pointer capitalize py-3 text-xl rounded-2xl"
+              >
+                Try It Now
+              </a>
+
               <div className="flex items-center gap-2">
                 <BsPlayCircle color="#F27405" size="2em" />
                 <p>Play Demo</p>
